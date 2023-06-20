7a5b8cdf618f76d22545215b2ab1de5a341a35a5
Emerson Franco <contato.emersonfranco@gmail.com>
2023-06-20 10:37:07 -0300

    Bug Teclado

--- app/src/main/java/com/example/rpgbox/MainActivity.java
+++ app/src/main/java/com/example/rpgbox/MainActivity.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("http://192.168.0.85:4200/");
 
 //        CustomTabsIntent.Builder customTabIntent = new CustomTabsIntent.Builder();
 //        customTabIntent.setToolbarColor(color.parseColor("#008000"))
@@ -27,11 +27,21 @@ public class MainActivity extends AppCompatActivity {
 
 
         WebSettings webSettings = myWebView.getSettings();
+        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        webSettings.setDomStorageEnabled(true);
+
+
 
         webSettings.setDomStorageEnabled(true);
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
 
         webSettings.setJavaScriptEnabled(true);
 
+
+        myWebView.loadUrl("http://192.168.0.85:4200/");
+
     }
+
+
 }
+
